0736cd203686ce31e2b7753f4e417f86166a94a8
Rahul <rahulsinghhh2312@gmail.com>
2023-08-25 22:39:50 +0530

    [feat] navbar added

---
+++ tt/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
